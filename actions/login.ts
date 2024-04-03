2e86b7d74c3ad293952bc28fe035b80b790deddf
mbrandow21 <masonb@dauntlessit.com>
2024-04-02 23:50:27 -0700

    Auth endpoints better equip

--- actions/login.ts
+++ actions/login.ts
@@ -3,6 +3,9 @@
 import * as z from "zod";
 
 import { LoginSchema } from "@/schemas";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values);
@@ -11,5 +14,26 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
     return { error: "Invalid Fields!" };
   }
 
-  return { success: "Email Sent!" }
+  const { email, password } = validatedFields.data;
+
+
+  try {
+    await signIn("credentials", {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT
+    })
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid Credentials!" }
+        default:
+          return { error: "Something went Wrong!" }
+      }
+    }
+
+    throw error;
+  }
+  return { error: "Something went wrong!" }
 };
--- actions/register.ts
+++ actions/register.ts
@@ -1,8 +1,11 @@
 "use server"
 
 import * as z from "zod";
+import { db } from "@/lib/db";
+import bcrypt from "bcryptjs"
 
 import { LoginSchema, RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -11,5 +14,24 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Invalid Fields!" };
   }
 
-  return { success: "Email Sent!" }
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email)
+
+  if (existingUser) {
+    return { error: "That email is already taken!" }
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword
+    }
+  });
+
+  //  TODO: Send Verificatio nToken Email.
+
+  return { success: "User Created!" }
 };
